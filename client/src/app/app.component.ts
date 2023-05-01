003a7dc1c7d1b6773986e41bcd5079568a095c24
atman-33 <gpbjk0304@gmail.com>
2023-05-01 14:41:20 +0900

    利用していない script-loader.service を削除

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-//import { ScriptLoaderService } from './services/script-loader.service';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Computing Atman';
 
-  constructor(
-    //private scriptLoaderService: ScriptLoaderService
-    ) {
-    //this.loadScript();
-  }
-
-  // async loadScript() {
-  //   await this.scriptLoaderService.loadScript('../assets/js/neumorphism.js');
-  //   console.log('Script loaded.');
-  // }
 }
