7aa0b9e898c9c0f27693412d501664e1a59cd24b
atman-33 <gpbjk0304@gmail.com>
2023-05-20 15:31:45 +0900

    バックエンドで、タグ・カテゴリ絞り込み、検索による記事一覧取得機能を追加

--- server/src/app/post/post.controller.ts
+++ server/src/app/post/post.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Get, Param, Query, Res } from '@nestjs/common';
 import { PostService } from './post.service';
 import { Post } from 'libs/src/shared/models/post.model';
 import { Response } from 'express';
@@ -9,26 +9,48 @@ export class PostController {
     constructor(private readonly postService: PostService) {
     }
 
-    @Get('ids')
-    async findAllIds(): Promise<string[]> {
-        return await this.postService.findAllIds();
-    }
-
     @Get()
-    async findAll(): Promise<Post[]> {
-        return await this.postService.findAll();
+    async findAll(@Query('page') page: number): Promise<Post[]> {
+        return await this.postService.findAll(page);
     }
 
-    @Get(':id')
+    @Get('items/:id')
     async findById(@Param('id') id: string): Promise<Post> {
         return await this.postService.findById(id);
     }
 
-    @Get('/img/:id/:fileName')
+    @Get('img/:id/:fileName')
     async getPostImageFile(
         @Param('id') id: string,
         @Param('fileName') fileName: string,
         @Res() res: Response) {
         return await this.postService.getPostImageFile(id, fileName, res);
+    }
+
+    @Get('ids')
+    async findAllIds(): Promise<string[]> {
+        return await this.postService.findAllIds();
+    }
+
+    @Get('categories/:categoryName')
+    async findCategoryPosts(
+        @Param('categoryName') categoryName: string,
+        @Query('page') page: number): Promise<Post[]> {
+        return await this.postService.findCategoryPosts(categoryName, page);
+    }
+
+    @Get('tags/:tagName')
+    async findTagPosts(
+        @Param('tagName') tagName: string,
+        @Query('page') page: number): Promise<Post[]> {
+        return await this.postService.findTagPosts(tagName, page);
+    }
+
+    @Get('search')
+    async searchPosts(
+        @Query('q') searchQuery: string, 
+        @Query('page') page: number): Promise<Post[]> {
+        const posts = await this.postService.searchPosts(searchQuery, page);
+        return posts;
     }
 }
--- server/src/app/post/post.service.ts
+++ server/src/app/post/post.service.ts
@@ -1,44 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { Post } from 'libs/src/shared/models/post.model';
-import { readFile, readdir } from 'fs';
-import { promisify } from 'util';
-import * as helpers from 'libs/src/shared/helpers';
 import { Response } from 'express';
+import { readFile, readdir } from 'fs';
+import * as constants from 'libs/src/shared/constants';
+import * as helpers from 'libs/src/shared/helpers';
+import { Post } from 'libs/src/shared/models/post.model';
+import * as utils from 'libs/src/shared/utils/index';
 import { join } from 'path';
+import { promisify } from 'util';
 
 @Injectable()
 export class PostService {
 
-  async findAllIds(): Promise<string[]> {
-
-    const folderPath = join(process.cwd(), 'dist/server/assets/posts');
-
-    try {
-      const dirents = await promisify(readdir)(
-        folderPath, {
-        withFileTypes: true,
-      });
-      const folders = dirents
-        .filter((dirent) => dirent.isDirectory())
-        .map((dirent) => dirent.name);
-      return folders;
-    } catch (error) {
-      console.error(`Failed to read directories: ${error}`);
-      throw new Error('Failed to read directories');
-    }
-  }
-
-  async findAll(): Promise<Post[]> {
-    const ids = await this.findAllIds();
-
-    const posts: Post[] = [];
-    for (const id of ids) {
-      posts.push(await this.findById(id));
-    }
+  /**
+   * ページに対応した記事データを取得
+   * @param page 
+   * @returns 
+   */
+  async findAll(page: number): Promise<Post[]> {
+    const allPosts = await this.getAllPosts();
+    const posts = this.getPagePosts(allPosts, page, constants.default.POSTS_PER_PAGE);
 
     return posts;
   }
 
+  /**
+   * 指定されたidの記事を取得
+   * @param id 
+   * @returns 
+   */
   async findById(id: string): Promise<Post> {
     // console.log(id);
 
@@ -52,12 +41,151 @@ export class PostService {
     }
   }
 
+  /**
+   * 画像を取得
+   * @param id 
+   * @param fileName 
+   * @param res 
+   * @returns 
+   */
   getPostImageFile(id: string, fileName: string, res: Response) {
 
     const imageFilePath = join(process.cwd(), 'dist/server/assets/posts', id, fileName);
     return res.sendFile(imageFilePath);
   }
 
+  /**
+   * 記事IDの一覧を取得
+   * @returns 
+   */
+  async findAllIds(): Promise<string[]> {
+    const folderPath = join(process.cwd(), 'dist/server/assets/posts');
+    try {
+      const dirents = await promisify(readdir)(
+        folderPath, {
+        withFileTypes: true,
+      });
+
+      const folders = dirents
+        .filter((dirent) => dirent.isDirectory())
+        .map((dirent) => dirent.name);
+      return folders;
+    } catch (error) {
+      console.error(`Failed to read directories: ${error}`);
+      throw new Error('Failed to read directories');
+    }
+  }
+
+  /**
+   * カテゴリでフィルタした記事一覧を取得
+   * @param categoryName 
+   * @param page 
+   * @returns 
+   */
+  async findCategoryPosts(categoryName: string, page: number): Promise<Post[]> {
+    let allPosts = await this.getAllPosts();
+
+    if (categoryName) {
+      allPosts = allPosts.filter(post => post.categories.includes(categoryName));
+    }
+
+    const posts = this.getPagePosts(allPosts, page, constants.default.POSTS_PER_PAGE);
+
+    return posts;
+  }
+
+  /**
+   * タグでフィルタした記事一覧を取得
+   * @param tagName 
+   * @param page 
+   * @returns 
+   */
+  async findTagPosts(tagName: string, page: number): Promise<Post[]> {
+    let allPosts = await this.getAllPosts();
+
+    if (tagName) {
+      allPosts = allPosts.filter(post => post.categories.includes(tagName));
+    }
+
+    const posts = this.getPagePosts(allPosts, page, constants.default.POSTS_PER_PAGE);
+
+    return posts;
+  }
+
+  /**
+   * 検索キーワードを含む記事を取得
+   * @param searchQuery 
+   * @param page 
+   * @returns 
+   */
+  async searchPosts(searchQuery: string, page: number): Promise<Post[]> {
+    let allPosts = await this.getAllPosts();
+
+    if (searchQuery) {
+      const searchTerms = searchQuery.toLowerCase().replace('　', ' ').split(' ');
+
+      allPosts = allPosts.filter(it => {
+        const titleMatch = searchTerms.every(term =>
+          it.title.toLowerCase().includes(term)
+        );
+        const articleMatch = searchTerms.every(term =>
+          it.article.toLowerCase().includes(term)
+        );
+        return titleMatch || articleMatch;
+      });
+    }
+
+    // console.log(`Posts count: ${allPosts.length}`);
+    const posts = this.getPagePosts(allPosts, page, constants.default.POSTS_PER_PAGE);
+    return posts;
+  }
+
+  /**
+   * 記事を全て取得
+   * @returns 
+   */
+  private async getAllPosts(): Promise<Post[]> {
+    const ids = await this.findAllIds();
+
+    let allPosts: Post[] = [];
+    for (const id of ids) {
+      allPosts.push(await this.findById(id));
+    }
+
+    allPosts = utils.sortByDate(allPosts, 'date', 'desc');  // 新規投稿順にソート
+
+    return allPosts;
+  }
+
+  /**
+   * ページサイズに区切った記事を取得
+   * @param allPosts 
+   * @param page 
+   * @returns 
+   */
+  private getPagePosts(allPosts: Post[], page: number, pageSize: number): Post[] {
+    if (page === undefined) {
+      page = 1;
+    }
+
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = page * pageSize;
+    const pagePosts = allPosts.slice(startIndex, endIndex); // ページ番号に対応したデータのIDの配列を取得
+
+    const posts: Post[] = [];
+    for (const post of pagePosts) {
+      posts.push(post);
+    }
+
+    return posts;
+  }
+
+  /**
+   * 記事ソースのmdファイルの中身をPostクラス構造に変換
+   * @param id 
+   * @param content 
+   * @returns 
+   */
   private parsePostContent(id: string, content: string): Post {
     // console.log(`id: ${id}`);
 
