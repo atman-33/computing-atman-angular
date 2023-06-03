21c29a3e20dbda5058b137961d9ce81a3b4f4c80
atman-33 <gpbjk0304@gmail.com>
2023-06-03 20:47:33 +0900

    frontend-サインアウト機能を復旧

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { BlogModule } from './blog/blog.module';
 import { TemplatesModule } from './templates/templates.module';
 import { TopPageComponent } from './top-page/top-page.component';
 
+
 /**
  * SPA用の画面遷移先（path）設定
  * @remark pathの先頭に/は不要（OK:detail NG:/detail）
